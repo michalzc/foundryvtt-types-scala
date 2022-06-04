d3e853422ba45b9997d32e1ab38e65ecc448f590
Michał Zając <michal.t.zajac@gmail.com>
2022-06-04 12:03:19 +0200

    Hooks added

---
+++ src/main/scala/foundryvtt/Hooks.scala
@@ -0,0 +1,9 @@
+package foundryvtt
+import scala.scalajs.js.annotation.JSGlobal
+import scalajs.js
+
+@js.native
+@JSGlobal
+object Hooks extends js.Object {
+  def once(callbackName: String, function: js.Function): Unit = js.native
+}
