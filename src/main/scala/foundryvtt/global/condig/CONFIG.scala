664da2ac55a948bf641a5870910804137a38cf8a
Michał Zając <michal.t.zajac@gmail.com>
2022-08-03 14:00:36 +0200

    Config object

---
+++ src/main/scala/foundryvtt/global/condig/CONFIG.scala
@@ -0,0 +1,11 @@
+package foundryvtt.global.condig
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSGlobal
+
+@js.native
+@JSGlobal
+object CONFIG extends js.Object {
+  def Actor: ActorConfig = js.native
+  def Item: ItemConfig   = js.native
+}
